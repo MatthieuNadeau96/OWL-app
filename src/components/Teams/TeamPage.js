414d481dc19083fa3f2b01fe63873702e7fd2065
Matthieu <matthieunadeau96@gmail.com>
2018-08-25 10:52:10 -0400

    added media queries to the team grid

--- src/components/Teams/TeamPage.js
+++ src/components/Teams/TeamPage.js
@@ -5,7 +5,6 @@ class TeamPage extends Component {
   render() {
     return (
       <div>
-        <h1>Teams:</h1>
         <div className="container">
           {
             this.props.teamList.length > 0 ? this.props.teamList.map(team => {
